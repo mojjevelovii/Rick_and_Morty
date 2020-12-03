ad2bb24523827064fb41e9bab93275ff8c5f4b1b
Veronika Shumilova <mojjevelovii@gmail.com>
2020-12-04 02:21:20 +0300

    - реализована основная разметка трех экранов - обеспечен переход с главного экрана по нажатию кнопки

--- app/src/main/java/ru/shumilova/rick_and_morty/mvp/view/general_screen/GeneralScreenFragment.kt
+++ app/src/main/java/ru/shumilova/rick_and_morty/mvp/view/general_screen/GeneralScreenFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_general_screen.*
 import ru.shumilova.rick_and_morty.R
@@ -24,6 +25,18 @@ class GeneralScreenFragment : Fragment() {
         Glide.with(view)
             .load(R.drawable.rm)
             .into(iv_main_picture)
+
+        btn_characters.setOnClickListener {
+            it.findNavController().navigate(R.id.action_general_screen_to_charactersFragment)
+        }
+
+        btn_locations.setOnClickListener {
+            it.findNavController().navigate(R.id.action_general_screen_to_locationsFragment)
+        }
+
+        btn_episodes.setOnClickListener {
+            it.findNavController().navigate(R.id.action_general_screen_to_episodesFragment)
+        }
     }
 
 }
